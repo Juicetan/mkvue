73c94bd2fb51fd0200f3210300f7380e1164012c
Justin <me@justinlam.ca>
2017-12-18 02:06:47 -0500

    Copy project template to project path;
    - create if path does not exist;

--- controllers/initializationController.js
+++ controllers/initializationController.js
@@ -1,7 +1,12 @@
+var fs = require('fs');
+var fse = require('fs-extra');
 
 var InitializationController = {
   setupProject: function(path){
-    
+    fse.ensureDirSync(path);
+    fse.copy(__dirname+'/../res/project',path).then(function(){
+      console.log('hrmmm');
+    })
   }
 };
 
