9c834312995457b6d510d2869d1928734efdfa29
Justin <me@justinlam.ca>
2017-12-21 09:49:10 -0500

    Enabled async usage;

--- controllers/initializationController.js
+++ controllers/initializationController.js
@@ -1,12 +1,18 @@
 var fs = require('fs');
 var fse = require('fs-extra');
+var Deferred = require('../models/deferred');
 
 var InitializationController = {
   setupProject: function(path){
+    var def = new Deferred();
+
     fse.ensureDirSync(path);
     fse.copy(__dirname+'/../res/project',path).then(function(){
-      console.log('hrmmm');
-    })
+      console.log('> project created');
+      def.resolve();
+    });
+
+    return def.promise;
   }
 };
 
