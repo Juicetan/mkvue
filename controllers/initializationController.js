383fbe546a0b00eba56a6840781aafe84d6b63c4
Justin <me@justinlam.ca>
2018-01-02 13:10:55 -0500

    Replace project stub project title in index;

--- controllers/initializationController.js
+++ controllers/initializationController.js
@@ -1,15 +1,20 @@
 var fs = require('fs');
 var fse = require('fs-extra');
 var Deferred = require('../models/deferred');
+var fsUtil = require('../utils/file');
 
 var InitializationController = {
   setupProject: function(path){
     var def = new Deferred();
+    var folderName = path.substring(0,path.lastIndexOf("\\")+1);
 
     fse.ensureDirSync(path);
     fse.copy(__dirname+'/../res/project',path).then(function(){
-      console.log('> project created');
-      def.resolve();
+      console.log('> project template created',path);
+
+      return fsUtil.replace(path+'/index.html','<!-- title -->','Test Project');
+    }).then(function(){
+      console.log('> project template initialized');
     });
 
     return def.promise;
