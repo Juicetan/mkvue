445e482614d14e929e5af52e9171ec01ae80c9c0
Justin <me@justinlam.ca>
2018-01-05 12:19:30 -0500

    Replace project name outlets with folder name;

--- controllers/initializationController.js
+++ controllers/initializationController.js
@@ -4,15 +4,22 @@ var Deferred = require('../models/deferred');
 var fsUtil = require('../utils/file');
 
 var InitializationController = {
+  replaceProjectName: function(path,projectName){
+    console.log('> setting project name',projectName);
+    return Promise.settle([
+      fsUtil.replace(path+'/index.html','<!-- title -->',projectName),
+      fsUtil.replace(path+'/package.json','<!-- title -->',projectName.toLowerCase().split(' ').join(''))
+    ]);
+  },
   setupProject: function(path){
     var def = new Deferred();
-    var folderName = path.substring(0,path.lastIndexOf("\\")+1);
+    var con = this;
+    var folderName = path.substring(path.lastIndexOf("/")+1) || 'Vue Project';
 
     fse.ensureDirSync(path);
     fse.copy(__dirname+'/../res/project',path).then(function(){
       console.log('> project template created',path);
-
-      return fsUtil.replace(path+'/index.html','<!-- title -->','Test Project');
+      return con.replaceProjectName(path,folderName);
     }).then(function(){
       console.log('> project template initialized');
     });
