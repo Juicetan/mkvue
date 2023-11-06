588cf242584451abcad25fc133cd48ce712a393e
Justin <me@justinlam.ca>
2023-11-06 15:05:19 -0600

    Added utility to turn strings into camelcase;

--- utils/string.js
+++ utils/string.js
@@ -10,6 +10,15 @@ var StrUtil = {
       return index == 0 ? letter.toLowerCase() : letter.toUpperCase();
     }).replace(/\s+/g, '');
   },
+  pascalize: function(str){
+    if (/^[a-z\d]+$/i.test(str)) {
+      return str.charAt(0).toUpperCase() + str.slice(1);
+    }
+    return str.replace(
+      /([a-z\d])([a-z\d]*)/gi,
+      (g0, g1, g2) => g1.toUpperCase() + g2.toLowerCase()
+    ).replace(/[^a-z\d]/gi, '');
+  },
   extractBlock: function(str, blockStart, blockEnd){
     var startSplit = str.split(blockStart);
     var endSplit = startSplit[1].split(blockEnd);
